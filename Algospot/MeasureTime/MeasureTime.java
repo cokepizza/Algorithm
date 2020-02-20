c4691388168a441790cafed93bf6afce125b1639
cokepizza <cokepizza3@gmail.com>
2020-02-20 18:56:01 +0900

    [code] MeasureTime 펜윅트리 최적화. 시간초과

--- Algospot/MeasureTime/MeasureTime.java
+++ Algospot/MeasureTime/MeasureTime.java
@@ -10,42 +10,40 @@ public class MeasureTime {
 	public static void main(String args[]) throws NumberFormatException, IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int C = Integer.parseInt(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		
 		for(int c=0; c < C; ++c) {
 			int N = Integer.parseInt(br.readLine());
 			int []A = new int [MAX_VALUE];
 			
 			StringTokenizer st = new StringTokenizer(br.readLine());
-			// MAX_VALUE만큼 배열을 선언했어도 펜윅트리 큰 그림은 2^n 형태라고 생각하면 될듯
+			int totalCost = 0;
+			
+			//	MAX_VALUE만큼 배열을 선언했어도 펜윅트리 큰 그림은 2^n 형태라고 생각하면 될듯
+			//	부분합을 이용해 num보다 큰 숫자들의 개수를 구한다 처음에 BST를 사용해볼까 했는데 펜윅이 더 좋은 듯 
 			for(int j=0; j<N; ++j) {
 				int pop = Integer.parseInt(st.nextToken());
+				int count = j;
 				
-				int count = 0;
+				//	num까지의 부분합
 				int num = pop;
 				while(num > 0) {
 					count -= A[num];
-					num -= num & (-num);
-				}
-				
-				num = MAX_VALUE-1;
-				while(num > 0) {
-					count += A[num];
-					num -= num & (-num);
+					num -= (num & -num);
 				}
 		
-				System.out.println(count);
+				totalCost += count;
 				
 				//	update
 				num = pop;
-				while(num < MAX_VALUE) {
-					A[num] += num;
-					num += num & (-num);	
+				while(num <= N) {
+					A[num] += 1;
+					num += (num & -num);	
 				}
 			}
 			
-			for(int j=0; j<6; ++j) {
-				System.out.println(A[j]);
-			}
+			sb.append(totalCost + "\n");
 		}
-		
+		System.out.print(sb.substring(0, sb.length() - 1));
 	}
 }
--- Baekjoon/src/Code16928/Code16928_BFS.java
+++ Baekjoon/src/Code16928/Code16928_BFS.java
@@ -6,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
-public class Code16928 {
+public class Code16928_BFS {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
