ad04665e168307a93db7364e1e5ed0fa16c0f328
cokepizza <cokepizza3@gmail.com>
2020-03-11 03:05:56 +0900

    [code] treap 구현

--- Algospot/Insertion/Insertion_BST_Treap2.java
+++ Algospot/Insertion/Insertion_BST_Treap2.java
@@ -1,6 +1,14 @@
 package Insertion;
 
 public class Insertion_BST_Treap2 {
+	public static class TreapPair {
+		Treap left, right;
+		public TreapPair (Treap left, Treap right) {
+			this.left = left;
+			this.right = right;
+		}
+	}
+	
 	public static class Treap {
 		Treap left, right;
 		int key, size;
@@ -30,22 +38,84 @@ public class Insertion_BST_Treap2 {
 			if(this.right != null) this.size += this.right.size;
 		};
 	}
-	
-	private static Treap insert(Treap root, Treap target) {
-		if(root == null) {
-			root = target;
-			return root;
-		}
+
+	private static TreapPair split(Treap root, int key) {
+		if(root == null) return new TreapPair(null, null);
 		
-		if(root.priority < target.priority) {
-			
-		} else if(root.key < target.key) {
-			root.setRight(insert(root.right, target));
+		if(root.key < key) {
+			TreapPair res = split(root.right, key);
+			root.setRight(res.left);
+			return new TreapPair(root, res.right);
+		}
+		TreapPair res = split(root.left, key);
+		root.setLeft(res.right);
+		return new TreapPair(res.left, root);
+	}
+	
+	//	root에 node삽입
+	private static Treap insert(Treap root, Treap node) {
+		if(root == null) return node;
+		
+		if(root.priority < node.priority) {
+			TreapPair splitted = split(root, node.key);
+		} else if(root.key < node.key) {
+			root.setRight(insert(root.right, node));
 		} else {
-			root.setLeft(insert(root.left, target));
+			root.setLeft(insert(root.left, node));
 		}
 		
 		return root;
+	}
+	
+	//	기본가정 max(a) < min(b)
+	private static Treap merge(Treap a, Treap b) {
+		if(a == null) return b;
+		if(b == null) return a;
+		
+		if(a.priority < b.priority) {
+			b.setLeft(merge(a, b.left));
+			return b;
+		}
+		a.setRight(merge(a.right, b));
+		return a;
+	}
+	
+	//	root에서 key삭제
+	private static Treap erase(Treap root, int key) {
+		if(root == null) return root;
+		
+		if(root.key == key) {
+			Treap remakeRoot = merge(root.left, root.right);
+			root = null;
+			return remakeRoot;
+		}
+		
+		if(key < root.key) {
+			root.setLeft(erase(root.left, key));
+		} else {
+			root.setRight(erase(root.right, key));
+		}
+		return root;
+	}
+	
+	//	크기순으로(중위순회) k번째 노드
+	private static Treap kth(Treap root, int k) {
+		int leftSize = 0;
+		if(root.left != null) leftSize = root.left.size;
+		if(k <= leftSize) return kth(root.left, k);
+		if(k == leftSize+1) return root;
+		return kth(root.right, k - leftSize - 1);
+	}
+	
+	//	key보다 작은 노드의 개수
+	private static int countLessThan(Treap root, int key) {
+		if(root == null) return 0;
+		
+		if(root.key >= key) {
+			return countLessThan(root.left, key);
+		}
+		int ls = (root.left != null) ? root.left.size : 0;
+		return ls + 1 + countLessThan(root.right, key);	
 	}
 
 	public static void main(String args[]) {
