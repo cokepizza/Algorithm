ec61c2bac0b70f5a9ce36d4e5eca53d271f2a624
cokepizza <cokepizza3@gmail.com>
2020-05-18 23:39:59 +0900

    [code] KaKao_Blind_Recruitment_2020_01. 기본적인 문자열 파싱 문제

---
+++ KaKao_Blind_Recruitment_2020/KaKao_Blind_Recruitment_2020_01/KaKao_Blind_Recruitment_2020_01.java
@@ -0,0 +1,47 @@
+package KaKao_Blind_Recruitment_2020_01;
+
+public class KaKao_Blind_Recruitment_2020_01 {
+	public static void main(String args[]) {
+//		int res = solution("aabbaccc");
+//		int res = solution("ababcdcdababcdcd");
+//		int res = solution("abcabcdede");
+//		int res = solution("abcabcabcabcdededededede");
+		int res = solution("xababcdcdababcdcd");
+		System.out.println(res);
+	}
+	
+	public static int solution(String s) {
+		int leng = s.length();
+		int answer = Integer.MAX_VALUE;
+		for(int i=1; i<=leng/2+1; ++i) {
+			String result = "";
+			String selected = s.substring(0, i);
+			int counter = 1;
+			for(int j=i; j<leng; j+=i) {
+				int maxLeng = Math.min(j+i, leng);
+				String pop = s.substring(j, maxLeng);
+				if(selected.equals(pop)) {
+					++counter;
+				} else {
+					if(counter == 1) {
+						result += selected;
+					} else {
+						result += String.valueOf(counter) + selected;
+					}
+					selected = pop;
+					counter = 1;
+				}
+			}
+			
+			if(counter == 1) {
+				result += selected;
+			} else {
+				result += String.valueOf(counter) + selected;
+			}
+			
+			answer = Math.min(answer, result.length());
+		}
+
+	    return answer;
+	}
+}
