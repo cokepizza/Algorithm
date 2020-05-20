7628ab83592826efbfcbf134f442c946abe29a53
cokepizza <cokepizza3@gmail.com>
2020-05-21 00:21:56 +0900

    [code] KaKao_Blind_Recruitment_2020_02. 재귀를 이용해 문자열 조합

---
+++ KaKao_Blind_Recruitment_2020/KaKao_Blind_Recruitment_2020_02/KaKao_Blind_Recruitment_2020_02.java
@@ -0,0 +1,78 @@
+package KaKao_Blind_Recruitment_2020_02;
+
+import java.util.LinkedList;
+
+public class KaKao_Blind_Recruitment_2020_02 {
+	public static void main(String args[]) {
+//		String res = solution("(()())()");
+//		String res = solution(")(");
+		String res = solution("()))((()");
+		System.out.println(res);
+	}
+	
+	public static String solution(String p) {
+		String answer = getCorrect(p);
+		return answer;
+	}
+	
+	private static String getCorrect(String p) {
+		int leng = p.length();
+		for(int i=2; i<=leng; i+=2) {
+			String u = p.substring(0, i);
+			String v = p.substring(i, leng);
+			if(isBalanced(u) && isBalanced(v)) {
+				if(isCorrect(u)) {
+					return u + getCorrect(v);
+				} else {
+					return "(" + getCorrect(v) + ")" + getReverse(u.substring(1, u.length()-1));
+				}
+			}
+		};
+		
+		return p;
+	}
+
+	private static String getReverse(String str) {
+		int leng = str.length();
+		StringBuilder sb = new StringBuilder();
+		for(int i=0; i<leng; ++i) {
+			char pop = str.charAt(i);
+			if(pop == '(') sb.append(')');
+			else if(pop == ')') sb.append('(');
+		}
+		
+		return sb.toString();
+	}
+
+	private static boolean isCorrect(String str) {
+		LinkedList <Character> queue = new LinkedList <>();
+		int leng = str.length();
+		for(int i=0; i<leng; ++i) {
+			char pop = str.charAt(i);
+			if(pop == '(') {
+				queue.add(pop);
+			} else {
+				if(queue.isEmpty()) return false;
+				char pick = queue.peekLast();
+				if(pick == '(') {
+					queue.pollLast();
+				} else {
+					return false;
+				}
+			}
+		}
+		return true;
+	}
+
+	private static boolean isBalanced(String str) {
+		int leng = str.length();
+		int open = 0, close = 0;
+		for(int i=0; i<leng; ++i) {
+			char pop = str.charAt(i);
+			if(pop == '(') ++open;
+			else if(pop == ')') ++close;
+		}
+		
+		return open == close;
+	}
+}
